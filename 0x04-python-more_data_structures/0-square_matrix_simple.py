e3a26887d6d79f5d01878f2158707a02d4c9d7ec
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2023-08-17 15:43:59 -0700

    corrections

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-<<<<<<< HEAD
     return [1** 2 for i in j] for j in matrix]
-=======
-	return [[i**2 for i in j] for j in matrix] 
->>>>>>> d0e7404eb82cde732ad2f88ff9b195155f84ebb0
