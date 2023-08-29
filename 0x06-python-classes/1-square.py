0cba9cb6261988e901e7fc4bc3f8ef64f6800102
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2023-08-29 15:26:37 -0700

    python class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""square class main"""
+
+
+class Square():
+    """square class"""
+
+    def __init__(self, size=None):
+        """instance of class Square
+
+        self.__size = size
