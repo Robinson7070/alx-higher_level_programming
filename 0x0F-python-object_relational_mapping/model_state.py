f4f9e28c86a3425585aa484d90b46eb4ebbbf82c
root <robinsonvictorchukwudi@gmail.com>
2023-11-18 14:09:46 -0800

    contains the class definition of a State and an instance base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pyton3
+"""contains class definition of a state and instance Base"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+"""Represent the base class for all tables"""
+
+
+class State(Base):
+    """Representation of a row in a states table"""
+    __tablename__ = 'states'
+    id = Column(Interger=True,
+                autoinrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
