1639cb615c6f10a7b7f05be3e2fc0ae6f6a3e20d
root <robinsonvictorchukwudi@gmail.com>
2023-11-18 12:15:12 -0800

    initial commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""lists all states from database 'hbtn_0_usa'"""
+"""lists all states from the database 'hbtn_0e_0_usa'"""
 
 import MySQLdb
 from sys import argv
 
-if __name__== "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
