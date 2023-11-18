2ce727a271581ea17e4bafcfb8bdb9046dcfe60d
root <robinsonvictorchukwudi@gmail.com>
2023-11-18 13:39:27 -0800

    lists all cities from the database 'hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument
-   and lists all cities of that state, using the database 
-   hbtn_0e_4_usa"""
+"""lists all cities from the database 'hbtn_0e_4_usa'"""
 
 import MySQLdb
 from sys import argv
@@ -9,6 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
+                         user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
@@ -17,5 +16,5 @@ if __name__ == "__main__":
                       ON cities.state_id = states.id""")
     for data in cursor.fetchall():
         print(data)
-        cursor.close()
-        db.close()
+    cursor.close()
+    db.close()
