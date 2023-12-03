57995e2cec3d1a5e1679781b625df9b303e4fc64
Robinson7070 <robinsonvictorchukwudi@gmail.com>
2023-12-03 04:03:46 -0800

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""lists the most recent 10 commits from a passed repository/owner"""
-import requests
+"""
+Python script that shows the last 10 commits of a repository
+in GitHub
+"""
+from requests import get, auth
 import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        repository_name = sys.argv[1]
-        owner_name = sys.argv[2]
-        api_url = 'https://api.github.com'
-        req_url = '{}/repos/{}/{}/commits?{}'.format(
-            api_url,
-            owner_name,
-            repository_name,
-            'per_page=10'
-        )
-        response = requests.get(
-            req_url,
-            headers={'Accept': 'application/vnd.github.v3+json'}
-        )
-        if response.status_code == 200:
-            for commit in response.json():
-                commit_sha = commit['sha']
-                commit_author = commit['commit']['author']['name']
-                print('{}: {}'.format(commit_sha, commit_author))
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
